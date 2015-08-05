409f6fedffe6a2107937d503390c7112eb811426
sam <sam@silver42.karoo.co.uk>
2015-08-05 14:24:14 +0100

    Use non-default font

--- demo/source/TextItem.hx
+++ demo/source/TextItem.hx
@@ -5,6 +5,6 @@ import flixel.text.FlxText;
 class TextItem extends FlxText {
 	public function new(x:Float = 0, y:Float = 0, text:String = "-", size:Int = 12) {
 		super(x, y, 0, text, size);
-		this.font = "arial";
+		this.font = "westa_seven";
 	}
 }
